c1cc53be9245c8d337289bbe9ca6e3fc56b16470
Jamie Epstein <jasepstein@gmail.com>
2021-01-26 16:56:22 -0600

    Set up server files and routes. Currently styling the page with Bootstrap.

---
+++ BGStockChecker/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+var data = require('../data/sample.json');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', data);
+});
+
+module.exports = router;
