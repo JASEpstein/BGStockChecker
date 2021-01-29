16263214d4246038dca1413dfc212ab5ee1455fa
Jamie Epstein <jasepstein@gmail.com>
2021-01-29 16:35:36 -0600

    Added Axios fetch function to App.js. Now need to find something to parse and search the DOM that gets returned.

--- BGStockChecker/app.js
+++ BGStockChecker/app.js
@@ -4,6 +4,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mustacheExpress = require('mustache-express');
 const cron = require('node-cron');
+const axios = require('axios');
+// var HTMLParser = require('node-html-parser');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,9 +25,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+const getDOM = async () => {
+    try {
+            return await axios.get('https://www.gamenerdz.com/deal-of-the-day')
+        } catch (error) {
+            console.error(error)
+        }
+}
+const readDOM = async () => {
+    const DOM = await getDOM()
+
+    const parsedDOM = HTMLParser.parse(DOM
+        // , {
+        // blockTextElements: {
+        //     script: true,	// keep text content when parsing
+        //     noscript: true,	// keep text content when parsing
+        //     style: true,		// keep text content when parsing
+        //     pre: true			// keep text content when parsing
+        // }
+        // }
+        );
+    if (parsedDOM) {
+        // console.log(DOM);
+        console.log(parsedDOM);
+    }
+}   
+readDOM();
+
+
+// CRON JOB CODE
 // Schedule tasks to be run on the server.
 // cron.schedule('* * * * *', function() {
-//     console.log('running a task every minute');
+//     console.log('DING DONG its six oclock PM!');
+
 // });
 
 module.exports = app;
