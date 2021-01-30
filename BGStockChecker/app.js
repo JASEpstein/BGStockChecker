4ad4020e6fc639a692f1dc3fe8720ee5808f3ac8
Jamie Epstein <jasepstein@gmail.com>
2021-01-29 20:29:41 -0600

    Tested some code to see if I can stop a cron.schedule within it... Might need another cron to restart it.

--- BGStockChecker/app.js
+++ BGStockChecker/app.js
@@ -31,35 +31,42 @@ const scrapeStatus = false;
 
 // CRON JOB CODE
 // Schedule tasks to be run on the server.
-// Runs every 5 mins from 11am to 2pm
-const task = cron.schedule('*/5 11-13 * * *', function() {    
-    //Axios Fetch code
-    axios.get(url)
-    .then((response) => {
-        let $ = cheerio.load(response.data);
+// Runs every 5 mins from 11am to 1pm
+// const task = cron.schedule('*/5 11-13 * * *', function() {    
+//     //Axios Fetch code
+//     axios.get(url)
+//     .then((response) => {
+//         let $ = cheerio.load(response.data);
         
-        let gameTitle = $('h4.card-title a').text();
-        console.log(gameTitle);
+//         let gameTitle = $('h4.card-title a').text();
+//         console.log(gameTitle);
 
-        let cartText = $('.cart_btn').text();   
-        console.log(cartText);
+//         let cartText = $('.cart_btn').text();   
+//         console.log(cartText);
 
-        let outOfStockBtn = $('.out_stock_btn').text();
-        console.log(outOfStockBtn);
+//         let outOfStockBtn = $('.out_stock_btn').text();
+//         console.log(outOfStockBtn);
 
-    })
-    .then((cartText, outOfStockBtn) => {
-        if(outOfStockBtn){
-            // Add code here for notifying front end that OOS
-            stopScrape();
-        }
+//     })
+//     .then((cartText, outOfStockBtn) => {
+//         if(outOfStockBtn){
+//             // Add code here for notifying front end that OOS
+//             stopScrape();
+//         }
 
-    })
-});
+//     })
+// });
+
+const task = cron.schedule('* * * * *', function(scrapeStatus){
+    console.log('CHECKAROONIE');
+    scrapeStatus = true;
+    stopScrape(scrapeStatus);
+})
 
 function stopScrape (scrapeStatus) {
     if(scrapeStatus == true){
         task.stop();
+        console.log('STOPPED');
     }
 }
 
