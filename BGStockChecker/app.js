c72d8f5d357da8667f2d72ef8b5cb60160422e55
Jamie Epstein <jasepstein@gmail.com>
2021-02-04 15:37:44 -0600

    Added a second cronjob to restart the first at midnight. Also adds some logic to write JSON data for the front end to use. Adds a JSON file to shuttle the data from server to front end.

--- BGStockChecker/app.js
+++ BGStockChecker/app.js
@@ -7,6 +7,9 @@ const cron = require('node-cron');
 const axios = require('axios');
 // var HTMLParser = require('node-html-parser');
 const cheerio = require('cheerio');
+const fs = require('fs');
+
+const frontEndVars = require('./data/frontEndVariables.json');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -32,42 +35,50 @@ const scrapeStatus = false;
 // CRON JOB CODE
 // Schedule tasks to be run on the server.
 // Runs every 5 mins from 11am to 1pm
-// const task = cron.schedule('*/5 11-13 * * *', function() {    
-//     //Axios Fetch code
-//     axios.get(url)
-//     .then((response) => {
-//         let $ = cheerio.load(response.data);
+const scrapeCron = cron.schedule('*/5 11-13 * * *', function() {    
+    //Axios Fetch code
+    axios.get(url)
+    .then((response) => {
+        let $ = cheerio.load(response.data);
         
-//         let gameTitle = $('h4.card-title a').text();
-//         console.log(gameTitle);
+        let gameTitle = $('h4.card-title a').text();
+        console.log(gameTitle);
 
-//         let cartText = $('.cart_btn').text();   
-//         console.log(cartText);
+        let cartText = $('.cart_btn').text();   
+        console.log(cartText);
 
-//         let outOfStockBtn = $('.out_stock_btn').text();
-//         console.log(outOfStockBtn);
+        let outOfStockBtn = $('.out_stock_btn').text();
+        console.log(outOfStockBtn);
 
-//     })
-//     .then((cartText, outOfStockBtn) => {
-//         if(outOfStockBtn){
-//             // Add code here for notifying front end that OOS
-//             stopScrape();
-//         }
+    })
+    .then((cartText, outOfStockBtn, gameTitle) => {
+        if(outOfStockBtn || !cartText){
+            //Stops checking the listing
+            scrapeCron.stop();
+            //Writes to JSON to update the front end
+            fs.writeFileSync(frontEndVars.stockStatus, "Out of Stock")
+        } else if(cartText){
+            //Add code for sending Product data to front end
+            fs.writeFileSync(frontEndVars.gameName, gameTitle);
 
-//     })
-// });
+        }
 
-const task = cron.schedule('* * * * *', function(scrapeStatus){
-    console.log('CHECKAROONIE');
-    scrapeStatus = true;
-    stopScrape(scrapeStatus);
+    })
+});
+
+
+const restartCron = cron.schedule('* 0 * * *', function(scrapeStatus){
+    
+    scrapeStatus = false;
+    scrapeCron.start();
 })
 
-function stopScrape (scrapeStatus) {
-    if(scrapeStatus == true){
-        task.stop();
-        console.log('STOPPED');
-    }
-}
+// function stopScrape (scrapeStatus) {
+//     if(scrapeStatus == true){
+//         scrapeCron.stop();
+//     }
+// }
+
+
 
 module.exports = app;
